66325ee529cc06b16c7d870ec3bb16b2b3b0d12f
HashtAggie <a.el.arras@gmail.com>
2016-04-18 16:32:32 +0200

    add category

--- db/migrate/20160418140149_create_products.rb
+++ db/migrate/20160418140149_create_products.rb
@@ -2,6 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
+      t.string :category
       t.text :description
       t.integer :price
       t.integer :quantity
