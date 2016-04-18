bee9b70a441f9c0222ca711c5f8085a07e68127a
noraeb <n.blomaard@gmail.com>
2016-04-18 17:32:52 +0200

    Database done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.create( name: "Dark chocolate bar", category: "Chocolate bar", description: "This dark chocolate bar is loaded with nutrients that can positively affect your health and you mood.", price: 3, quantity: 1000, image: "darkbar.jpg" )
+Product.create( name: "Milk chocolate bar", category: "Chocolate bar", description: "Enjoy this delicious chocolate bar to heal your broken heart after your boyfriend has dumped you.", price: 3, quantity: 1000, image: "milkchocolate.jpg" )
+Product.create( name: "White chocolate bar", category: "Chocolate bar", description: "We all know this is not real chocolate but who cares! It's soooo damn tasty!", price: 3, quantity: 800, image: "whitechocolate.jpg")
+Product.create( name: "Sea salt caramel chocolate bar", category: "Chocolate bar", description: "This is our personal favourite, you should try it yourself!", price: 4, quantity: 750, image: "seasalt.jpg")
+Product.create( name: "Chocolate milk and rum", category: "Chocolate milk", description: "Best of both worlds :)", price: 7, quantity: 400, image: "chocolatemilk.jpg" )
+Product.create( name: "Dark chocolate milk", category: "Chocolate milk", description: "On a cold winter day warm yourself with this liquid gold.", price: 6, quantity: 100, image: "chocolatemilk2.jpg" )
+Product.create( name: "Skinny chocolate milk", category: "Chocolate milk", description: "For all you skinny bitches out there.", price: 8, quantity: 200, image: "chocolatemilk3.jpg" )
+Product.create( name: "Strawberries", category: "Chocolate covered fruit", description: "10 super fresh sweet chocolate covered strawberries.", price: 8, quantity: 30, image: "strawberry.jpg" )
+Product.create( name: "Cherries", category: "Chocolate covered fruit", description: "10 super fresh sweet chocolate covered cherries.", price: 8, quantity: 30, image: "cherry.jpg" )
+Product.create( name: "Banana", category: "Chocolate covered fruit", description: "10 super fresh sweet chocolate covered bananas.", price: 8, quantity: 30, image: "banana.jpg" )
+Product.create( name: "Hazelnuts", category: "Going nuts", description: "The perfect combination of crunchy hazelnuts covered in the best chocolate." , price: 5, quantity: 46, image: "hazelnuts.png" )
+Product.create( name: "Pecans", category: "Going nuts", description: "The perfect combination of crunchy pecans covered in the best chocolate.", price: 5, quantity: 88, image: "pecans.jpg")
+Product.create( name: "Almonds", category: "Going nuts", description: "The perfect combination of crunchy almonds covered in the best chocolate.",  price: 5, quantity: 99, image: "almonds.jpg" )
+Product.create( name: "Peanuts", category: "Going nuts", description: "The perfect combination of crunchy peanuts covered in the best chocolate.", price: 5, quantity: 101, image: "peanuts.jpg" )
